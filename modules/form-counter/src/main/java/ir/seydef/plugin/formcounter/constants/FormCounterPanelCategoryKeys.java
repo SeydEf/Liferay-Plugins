1dbc9715b58075d54007538ee85e6727357ebfd1
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-09-25 22:30:38 +0330

    feat: implement form counter panel app config

---
+++ modules/form-counter/src/main/java/ir/seydef/plugin/formcounter/constants/FormCounterPanelCategoryKeys.java
@@ -0,0 +1,10 @@
+package ir.seydef.plugin.formcounter.constants;
+
+/**
+ * @author seydef
+ */
+public class FormCounterPanelCategoryKeys {
+
+	public static final String CONTROL_PANEL_CATEGORY = "FormCounter";
+
+}
