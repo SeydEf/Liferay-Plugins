ef87e69c859708e65fe2e39d104b5e004b33d5eb
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-09-27 19:25:51 +0330

    feat: implement services method that necessary for work with forms and records and keys(FormCounterPortletKeys)

---
+++ modules/form-counter/src/main/java/ir/seydef/plugin/formcounter/application/list/FormCounterPanelApp.java
@@ -0,0 +1,39 @@
+package ir.seydef.plugin.formcounter.application.list;
+
+import ir.seydef.plugin.formcounter.constants.FormCounterPanelCategoryKeys;
+import ir.seydef.plugin.formcounter.constants.FormCounterPortletKeys;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.portal.kernel.model.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author S.Abolfazl Eftekhari
+ */
+@Component(
+	property = {
+		"panel.app.order:Integer=100",
+		"panel.category.key=" + FormCounterPanelCategoryKeys.CONTROL_PANEL_CATEGORY
+	},
+	service = PanelApp.class
+)
+public class FormCounterPanelApp extends BasePanelApp {
+
+	@Override
+	public String getPortletId() {
+		return FormCounterPortletKeys.FORMCOUNTER;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + FormCounterPortletKeys.FORMCOUNTER + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
---
+++ modules/form-counter/src/main/java/ir/seydef/plugin/formcounter/constants/FormCounterPortletKeys.java
@@ -0,0 +1,21 @@
+package ir.seydef.plugin.formcounter.constants;
+
+/**
+ * @author S.Abolfazl Eftekhari
+ */
+public class FormCounterPortletKeys {
+
+	public static final String FORMCOUNTER = "ir_seydef_plugin_formcounter_FormCounterPortlet";
+
+	public static final String PARAM_FORM_INSTANCE_ID = "formInstanceId";
+	public static final String PARAM_DELTA = "delta";
+
+	public static final int DEFAULT_DELTA = 20;
+	public static final String DEFAULT_ORDER_BY_COL = "createDate";
+	public static final String DEFAULT_ORDER_BY_TYPE = "desc";
+
+	public static final String DDM_FIELD_BRANCH_ID = "branchId";
+
+	public static final String USER_EXPANDO_BRANCH_ID = "branchId";
+
+}
